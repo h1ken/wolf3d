b3b261f51ec786cfcfb7f80469dea81bf225c63b
Igor Korneev <rokvin@gmail.com>
2020-03-03 12:57:44 +0300

    Fixed loop, plus "start" of reycasting

--- src/game_loop.c
+++ src/game_loop.c
@@ -16,24 +16,28 @@ static int	check_events(t_wolf3d *wolf, t_sdl_info *i_sdl)
 {
 	SDL_Event	e;
 
-		while (SDL_PollEvent(&e) != 0)
-		{
-			if (e.type == SDL_QUIT)
-				return (0);
-			else if (e.type == SDL_KEYDOWN)
-				return (key_press_handle(e.key, wolf));
-			else if (e.type == SDL_KEYUP)
-				return (key_up_handle(e.key, wolf));
-		}
+	while (SDL_PollEvent(&e) != 0)
+	{
+		if (e.type == SDL_QUIT)
+			return (0);
+		else if (e.type == SDL_KEYDOWN)
+			return (key_press_handle(e.key, wolf));
+		else if (e.type == SDL_KEYUP)
+			return (key_up_handle(e.key, wolf));
+	}
+	return (1);
 }
 
 void		loop(t_wolf3d *wolf, t_sdl_info *i_sdl)
 {
-	int			x;
+	int	x;
+	float	dirX, dirY;
 
 	wolf->w_surf = SDL_GetWindowSurface(i_sdl->w);
 	x = 1;
 	SDL_UpdateWindowSurface(i_sdl->w);
+	dirX = -1;
+	dirY = 0;
 	while (x)
 	{
 		x = check_events(wolf, i_sdl);
