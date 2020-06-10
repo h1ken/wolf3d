d817c73881568286ecd60c3469ad0d2119fa1ee8
h1ken <rokvin@gmail.com>
2020-06-10 16:15:27 +0300

    torch done

--- src/black_rock_shooter.c
+++ src/black_rock_shooter.c
@@ -6,7 +6,7 @@
 /*   By: h1ken <h1ken@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 16:31:02 by cstripeb          #+#    #+#             */
-/*   Updated: 2020/06/09 18:23:54 by h1ken            ###   ########.fr       */
+/*   Updated: 2020/06/10 16:14:56 by h1ken            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,18 @@ void    draw_weapon(t_sdl_info *isdl, t_wolf3d *wolf)
 
 	i = 0;
 	texture = (int *)wolf->textures->objects[5]->pixels;
-	while (i < 36)
+	while (i < 34)
 	{
 		j = 0;
-		while (j <= 120)
+		while (j < 80)
 		{
-			color = *(texture + i + 36 * x + j * 338);
+			color = *(texture + i + 34 * x + j * 307);
 			if ((color & 0x00FFFFFF) != 0)
 				put_pixel_to_surf(370 + i, 370 + j,
 									wolf->w_surf, color);
 			j++;
 		}
 		i++;
-		
 	}
 	j = SDL_GetTicks();
 	x = j / 150 % 9;
